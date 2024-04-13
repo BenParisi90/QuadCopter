5e52b630810b8781981c0a2f538b1200a8a2f1cb
Ben Parisi <BenParisi90@gmail.com>
2024-04-13 09:40:20 -0700

    output pot pin value to serial monitor every loop

--- QuadCopter/src/main.cpp
+++ QuadCopter/src/main.cpp
@@ -3,15 +3,22 @@
 // put function declarations here:
 //int myFunction(int, int);
 
+#define POT_PIN 2
+
 void setup() {
   // put your setup code here, to run once:
   //int result = myFunction(2, 3);
   Serial.begin(9600);
+
+  pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World");
+  //Serial.println("Hello World");
+
+  int potValue = analogRead(POT_PIN);
+  Serial.println(potValue);
 }
 
 // put function definitions here:
